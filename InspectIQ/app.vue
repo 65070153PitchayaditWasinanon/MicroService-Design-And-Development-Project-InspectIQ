c5a976edf4a05a2a52e136ca78269765994ee931
Nontakorn Pumloyfa 65070112 <65070112@kmitl.ac.th>
2025-03-07 22:24:52 +0700

    navbar

--- InspectIQ/app.vue
+++ InspectIQ/app.vue
@@ -1,25 +1,3 @@
-<script setup>
-import { useHead } from 'unhead'
-
-useHead({
-  title: 'Inance',
-  meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
-    { httpEquiv: 'X-UA-Compatible', content: 'IE=edge' },
-    { name: 'keywords', content: '' },
-    { name: 'description', content: '' },
-    { name: 'author', content: '' }
-  ],
-  link: [
-    { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css' },
-    { rel: 'stylesheet', href: '/css/bootstrap.css' },
-    { rel: 'stylesheet', href: '/css/font-awesome.min.css' },
-    { rel: 'stylesheet', href: '/css/style.css' },
-    { rel: 'stylesheet', href: '/css/responsive.css' }
-  ]
-})
-</script>
 
 <template>
   <NuxtPage />
--- InspectIQ/pages/index.vue
+++ InspectIQ/pages/index.vue
@@ -1,14 +1,35 @@
 <template>
-  <div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-</div>
+  <div class="hero_area">
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 ">
 
-<button type="button" class="btn btn-dark">Dark</button>
+      <div class="container-fluid d-flex justify-content-between align-items-center  ">
+        <!-- โลโก้ -->
+        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+          <h2 class="logotext m-0 text-primary">INSPECT IQ</h2>
+        </a>
+
+        <!-- เมนูหลัก -->
+        <div class="collapse navbar-collapse ms-3" id="navbarCollapse">
+          <div class="navbar-nav">
+            <a href="#" class="nav-item nav-link active">Home</a>
+            <a href="#" class="nav-item nav-link">About</a>
+            <a href="#" class="nav-item nav-link">PROFILE</a>
+          </div>
+        </div>
+
+        <!-- ปุ่ม LOGOUT -->
+        <div class="logout-btn ms-auto">
+          <a href="" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">LOGOUT <i class="fa fa-arrow-right ms-3"></i></a>
+        </div>
+
+        <!-- ปุ่ม Toggle Navbar (สำหรับมือถือ) -->
+        <button class="navbar-toggler me-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+
+    </nav>
+  </div>
 </template>
 
 <script setup>
@@ -17,5 +38,6 @@
 
 <style scoped>
 /* นำเข้า CSS */
+
 @import '@/assets/css/style.css'; /* ใช้ path ที่ถูกต้อง */
 </style>
