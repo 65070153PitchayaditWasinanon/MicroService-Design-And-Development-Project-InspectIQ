fe86e5423ca597e3065948ffe6adb4ca65a3057d
Nontakorn Pumloyfa 65070112 <65070112@kmitl.ac.th>
2025-03-07 21:01:56 +0700

    start pages and assets

--- InspectIQ/nuxt.config.ts
+++ InspectIQ/nuxt.config.ts
@@ -1,5 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
+// nuxt.config.js
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    'bootstrap/dist/css/bootstrap.min.css',  // นำเข้า CSS ของ Bootstrap
+    'font-awesome/css/font-awesome.min.css'  // นำเข้า Font Awesome
+  ]
 })
